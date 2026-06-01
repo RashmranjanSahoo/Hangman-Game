66d150f457691e85f8026ef4e957877f649a00e9
rashmi ranjan <rsahoorashmi@gmail.com>
2026-06-01 14:20:16 +0530

    Completed Hangman game frontend UI

---
+++ app.js
@@ -0,0 +1,7 @@
+const keyboard = document.querySelector(".keyboard");
+
+for(let i = 97; i <= 122; i++) {
+    const button = document.createElement("button");
+    button.innerText = String.fromCharCode(i);
+    keyboard.appendChild(button);
+}
